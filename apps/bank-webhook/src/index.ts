33c38bcded27f2b526e3dab6b0f8a0b1a29cef0e
Asher <sharmaaman0202@gmail.com>
2025-08-07 07:31:38 +0530

    Test ci rebuild a

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -19,7 +19,6 @@ app.post("/hdfcWebhook", async (req, res) => {
     };
 
     try {
-        console.log("parsed body", paymentInformation.token, paymentInformation.userId, paymentInformation.amount);
         await db.$transaction([
             db.balance.updateMany({
                 where: {
